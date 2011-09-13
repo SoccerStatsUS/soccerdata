2369c53c0989384a7498ff44f8ad6d860490b715
Chris Edgemon <chrisedgemon@gmail.com>
2011-09-13 02:43:13 -0400

    Fix game view, add nasl and mls game views.

--- controller.py
+++ controller.py
@@ -27,11 +27,26 @@ app.config.from_pyfile('settings.cfg')
 def index():
     return render_template("index.html")
 
-@app.route("/g/<collection>")
-def game_page(collection):
-    rows = mongo.get_rows(collection)
+@app.route("/g/games")
+def game_page():
     rows = mongo.get_rows('statto.games')[:100]
     return render_template("game.html", rows=rows)
+
+@app.route("/g/spain")
+def spain_page():
+    rows = mongo.get_rows('rsssf-spain-games')
+    return render_template("game.html", rows=rows)
+
+@app.route("/g/nasl")
+def nasl():
+    rows = mongo.get_rows('nasl-scores')
+    return render_template("game.html", rows=rows)    
+
+@app.route("/g/mls")
+def mls():
+    rows = mongo.get_rows('rsssf-mls-games')
+    return render_template("game.html", rows=rows)    
+
 
 @app.route("/<url>")
 def flatpage(url):
--- mongo.py
+++ mongo.py
@@ -22,14 +22,18 @@ def insert_row(collection, row):
 def insert_rows(collection, rows):
     c = db[collection]
     for row in rows:
-        c.insert(row)
+        try:
+            c.insert(row)
+        except:
+            import pdb; pdb.set_trace()
 
 def get_rows(collection):
     c = db[collection]
-    return [row for row in c.find()]
+    return [row for row in c.find().sort('date', pymongo.ASCENDING)]
 
 
-def delete_rows(collection, rows):
+def delete_rows(collection, rows=None):
+    c = db[collection]
     if rows is None:
         rows = c.find()
     c = db[collection]
@@ -40,6 +44,8 @@ def delete_rows(collection, rows):
 def scrape_statto(date):
     rows = statto.process_date(date)
     insert_rows("statto.games", rows)
+    # Statto is scraping all possible games for a given date,
+    # e.g. 2011-3-21.
     insert_row("statto.games.dates", {"date": date})
 
 def run_statto():
@@ -50,3 +56,36 @@ def run_statto():
         next_date = datetime.datetime.today() - datetime.timedelta(days=1)
     print next_date
     scrape_statto(next_date)
+
+
+def scrape_spain(years=None):
+    from scrapers.rsssf import spain
+
+    if years is None:
+        years = range(2000, 2012)
+
+    delete_rows("rsssf-spain-games")
+    for year in years:
+        rows = spain.process_year(year)
+        insert_rows("rsssf-spain-games", rows)
+
+
+def scrape_mls(years=None):
+    from scrapers.rsssf import usa
+
+    if years is None:
+        years = range(1996, 2009)
+
+    delete_rows("rsssf-mls-games")
+    for year in years:
+        rows = usa.process_year(year)
+        insert_rows("rsssf-mls-games", rows)
+
+
+def scrape_nasl():
+    from scrapers import nasl
+    scores = nasl.scrape_scores()
+    delete_rows("nasl-scores")
+    insert_rows("nasl-scores", scores)
+        
+
