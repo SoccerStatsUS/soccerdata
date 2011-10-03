5e545b82ff50a637e4f6b1e875883d8191220776
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-03 05:00:05 -0400

    Move old scraper tests into main scraper/tests.py, create cache.py for data cache functions.

---
+++ cache.py
@@ -0,0 +1,105 @@
+import functools
+import hashlib
+import pymongo
+
+# The cache should be implemented with redis instead of mongo!
+# Automate installation of mongo and redis?
+
+connection = pymongo.Connection()
+cache_db = connection.cache
+
+class UncachedException(Exception): pass
+
+
+def set_data_cache(cache_id, value):
+    """
+    Set a value in the cache.
+    """
+    cache_db.data_cache.remove({
+            "cache_id": cache_id,
+            })
+
+    cache_db.data_cache.insert({
+            "cache_id": cache_id,
+            "value": value
+            })
+
+
+
+def get_data_cache(cache_id):
+    """
+    Get a value from cache.
+    """
+    d = cache_db.data_cache.find_one({
+            'cache_id': cache_id
+            })
+    if d:
+        return d['value']
+    else:
+        raise UncachedException()
+
+
+class AbstractCache(object):
+    """Decorator that caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned, and
+    not re-evaluated.
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        return functools.partial(self.__call__, obj)
+
+
+
+class data_cache(AbstractCache):
+    """
+    A cache decorator that uses mongodb as a backend.
+    """
+
+    def __call__(self, *args):
+        # Should maybe fail rather than returning None?
+        # Presumably need kwargs in here too?
+        key = hashlib.md5('%s:%s' % (self.func.func_name, args)).hexdigest()
+
+        try:
+            data = get_data_cache(key)
+            print "Returning %s%s from data cache." % (self.func.func_name, args)
+            return data
+        except UncachedException:
+            pass
+
+        value = self.func(*args)
+        try:
+            set_data_cache(key, value)
+        except:
+            import pdb; pdb.set_trace()
+            x = 5
+
+        return value
+
+
+class set_cache(AbstractCache):
+    """
+    A decorator that will set the cache without trying to access it.
+    """
+    # Use this for overwriting old cache values.
+
+    def __call__(self, *args):
+        # Should maybe fail rather than returning None?
+        # Presumably need kwargs in here too?
+
+        value = self.func(*args)
+        set_cache(key, value)
+        return value
+
+                                    
+
--- utils.py
+++ utils.py
@@ -1,36 +1,14 @@
-import bson
-import functools
-import hashlib
 import pymongo
 import random
-import requests
 import time
 import urllib2
 
 from BeautifulSoup import BeautifulSoup
 
-
-# This should be implemented with redis instead of mongo!
-# Automate installation of mongo and redis?
-# Document!
-
+from soccerdata.cache import data_cache, set_cache
 
 connection = pymongo.Connection()
-
-# Should be connection.cache
-scraper_db = connection.scraper
-
-cache_db = connection.cache
-
-# Should be db.page_cache
-
 USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13'
-
-
-class UncachedException(Exception): pass
-
-
-
 
 def get_contents(l):
     """
@@ -57,98 +35,7 @@ def dict_to_str(d):
     return "?" + "&".join(opts)
 
 
-
-class AbstractCache(object):
-    """Decorator that caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned, and
-    not re-evaluated.
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        raise NotImplementedError()
-
-    def __repr__(self):
-        """Return the function's docstring."""
-        return self.func.__doc__
-    def __get__(self, obj, objtype):
-        """Support instance methods."""
-        return functools.partial(self.__call__, obj)
-
-
-
-class data_cache(AbstractCache):
-    """
-    A cache decorator that uses mongodb as a backend.
-    """
-
-    def __call__(self, *args):
-        # Should maybe fail rather than returning None?
-        # Presumably need kwargs in here too?
-        key = hashlib.md5('%s:%s' % (self.func.func_name, args)).hexdigest()
-
-        try:
-            data = get_data_cache(key)
-            print "Returning %s%s from data cache." % (self.func.func_name, args)
-            return data
-        except UncachedException:
-            pass
-
-        value = self.func(*args)
-        try:
-            set_data_cache(key, value)
-        except:
-            import pdb; pdb.set_trace()
-            x = 5
-
-        return value
-
-
-class set_cache(AbstractCache):
-    """
-    A decorator that will set the cache without trying to access it.
-    """
-    # Use this for overwriting old cache values.
-
-    def __call__(self, *args):
-        # Should maybe fail rather than returning None?
-        # Presumably need kwargs in here too?
-
-        value = self.func(*args)
-        set_cache(key, value)
-        return value
-
-
-def set_data_cache(cache_id, value):
-    """
-    Set a value in the cache.
-    """
-    cache_db.data_cache.remove({
-            "cache_id": cache_id,
-            })
-
-    cache_db.data_cache.insert({
-            "cache_id": cache_id,
-            "value": value
-            })
-
-
-
-def get_data_cache(cache_id):
-    """
-    Get a value from cache.
-    """
-    d = cache_db.data_cache.find_one({
-            'cache_id': cache_id
-            })
-    if d:
-        return d['value']
-    else:
-        raise UncachedException()
-                                    
-    
+# Scrapers
 
 
 #def scrape_post(url, options_dict):
@@ -196,7 +83,9 @@ def scrape_url(url, refresh=False, encoding=None, sleep=0):
     if encoding is None:
         encoding = 'utf-8'
 
-    # Should be cache_db.page_cache
+    # Should be connection.cache
+    scraper_db = connection.scraper
+
     pages_collection = scraper_db.pages
 
 
