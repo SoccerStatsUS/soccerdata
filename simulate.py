a41a675227fe2332efb53a694d3e85a915eae3bf
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-06 14:07:09 -0400

    Add simulate.py

---
+++ simulate.py
@@ -0,0 +1,21 @@
+
+# Markov chains?
+
+# http://www.nytimes.com/2009/04/07/science/07diam.html
+
+# http://www.lehigh.edu/~dmd1/apba3.pdf
+
+# http://archive.ite.journal.informs.org/Vol5No1/Sokol/Sokol.pdf
+
+# http://en.wikipedia.org/wiki/Statistical_association_football_predictions
+
+# http://www.scibet.com/
+
+# http://www.crediblesport.com/game-analysis/prediction-models-used-to-determine-football-scores/
+
+"""
+Baseball uses markov chains to simulate transitions between inning states.
+
+The only similar thing that I can do is simulate each minute...
+
+"""
