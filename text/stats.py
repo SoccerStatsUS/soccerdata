74e414f996a301c06f295ae23cfaed04a9eeeb6d
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-21 12:55:29 -0400

    Updates to text/stats to use new stats files for usl, nasl, misl; various code fixes.

--- text/stats.py
+++ text/stats.py
@@ -9,18 +9,23 @@ DIR = '/home/chris/www/soccerdata/data/stats'
 if not os.path.exists(DIR):
     DIR = "/Users/chrisedgemon/www/soccerdata/data/stats"
 
-#@data_cache
-def process_all_chris_stats():
 
+def process_mls_stats():
+    return process_stats("mls_stats.csv", "Major League Soccer")
+
+def process_nasl_stats():
+    return process_stats("naslmisl.csv", format_name=False)
+
+#@data_cache
+def process_usl_stats():
     l = []
-    l.extend(process_stats("mls_stats.csv", "MLS"))
-    l.extend(process_stats("pdl_stats.csv", "PDL"))
-    l.extend(process_stats("psl_stats.csv", "PSL"))
-    l.extend(process_stats("usl_one_stats.csv", "USL-1"))
+    l.extend(process_stats("usl1_19972005.csv", "USL First Division"))
+    l.extend(process_stats("usl1_20062007.csv", "USL First Division"))
+    l.extend(process_stats("usl1_20082009.csv", "USL First Division"))
+    l.extend(process_stats("psl_stats.csv", "USL Second Division"))
+    l.extend(process_stats("pdl_stats.csv", "USL Premier Developmental League"))
     return l
     
-
-
 def process_name(s):
     if ',' not in s:
         return s
@@ -37,31 +42,69 @@ def process_name(s):
             s = mapping[s]
 
     try:
-        last, first = name = s.split(',') # Assume no 2-comma names.    
+        last, first = [e.strip() for e in s.split(',')] # Assume no 2-comma names.    
     except:
         import pdb; pdb.set_trace()
-    name = first + ' ' + last    
+    name = first + ' ' + last
     return name.strip()
 
 
 
-def process_stats(fn, competition):
+def process_stats(fn, competition=None, format_name=True):
 
+    def fixes(d):
+        # Incorrect line in naslmisl.csv
+        if d.get('name') == 'Santiago Formoso':
+            if d.get('games_played') == 'D':
+                d.update({'games_played': '5', 'goals': '0'})
+        return d
+                         
     def process_line(line):
         # clean this up.
+
+        if not line.strip():
+            return {}
+
         fields = line.split('\t')
         d = dict(zip(header, fields))
-        d['name'] = process_name(d['name'])
-        d['competition'] = competition
+        d = fixes(d)
+        
+
+        if 'name' not in d:
+            return {}
+
+        if not d['name']:
+            return {}
+        
+
+
+        if format_name:
+            d['name'] = process_name(d['name'])
+
+        if competition is not None:
+            d['competition'] = competition
+
         d['team'] = d['team']
-        d['season'] = d.pop('year')
+
+        if 'year' in d:
+            d['season'] = d.pop('year')
+        else:
+            d['season'] = d['season']
+
+
+
+        if 'games' in d:
+            d['games_played'] = d.pop('games')
             
         for k in 'games_played', 'games_started', 'minutes', 'goals', 'assists', 'shots', 'shots_on_goal', \
                 'blocks', 'fouls_committed', 'fouls_suffered', 'offsides', 'pk_goals', 'pk_attempts', 'pks_drawn', \
                 'pks_committed':
             if k in d:
                 v = d[k]
-                if v == '':
+                # Blank character showing up in naslmisl.csv
+                v = v.replace('\xc2\xa0', '')
+
+                if v in ('', '-'):
                     v = 0
                 elif v == '?':
                     pass
@@ -80,12 +123,10 @@ def process_stats(fn, competition):
     path = os.path.join(DIR, fn)
     lines = open(path).read().strip().split('\n')
     header = lines[0].split('\t')
-    return [process_line(line) for line in lines[1:]]
+    l = [process_line(line) for line in lines[1:]]
+    return [e for e in l if e]
 
 
-def load_chris_stats():
-    return process_stats(STATS_PATH)
-        
 if __name__ == "__main__":
-    process_all_chris_stats()
-
+    print process_nasl_stats()
+    
