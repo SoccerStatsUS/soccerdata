584cbfa87fd275be2b154deb64ea90e2f219b189
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-21 12:57:04 -0400

    Add award lists for misl, nasl.

---
+++ data/lists/misl.py
@@ -0,0 +1,18 @@
+mvp = [
+    ('1978-1979', 'Steve Zungul'),
+    ('1979-1980', 'Steve Zungul'),
+    ('1980-1981', 'Steve Zungul'),
+    ('1981-1982', 'Steve Zungul'),
+    ('1981-1982', 'Stan Terlecki'),
+    ('1982-1983', 'Alan Meyer'),
+    ('1983-1984', 'Stan Stamenkovic'),
+    ('1984-1985', 'Steve Zungul'),
+    ('1985-1986', 'Steve Zungul'),
+    ('1986-1987', 'Tatu'),
+    ('1987-1988', 'Erik Rasmussen'),
+    ('1988-1989', 'Preki'),
+    ('1989-1990', 'Tatu'),
+    ('1990-1991', 'Victor Nogueira'),
+    ('1991-1992', 'Victor Nogueira'),
+    ]
+    
---
+++ data/lists/nasl.py
@@ -0,0 +1,42 @@
+mvp = [
+    (1968, 'John Kowallik'),
+    (1969, 'Cirillo Fernandez'),
+    (1970, 'Carlos Metidieri'),
+    (1971, 'Carlos Metidieri'),
+    (1972, 'Randy Horton'),
+    (1973, 'Warren Archibald'),
+    (1974, 'Peter Silvester'),
+    (1975, 'Steve David'),
+    (1976, 'Pele'),
+    (1977, 'Franz Beckenbauer'),
+    (1978, 'Mike Flanagan'),
+    (1979, 'Johan Cruyff'),
+    (1980, 'Roger Davies'),
+    (1981, 'Giorgio Chinaglia'),
+    (1982, 'Peter Ward'),
+    (1983, 'Roberto Cabanas'),
+    (1984, 'Steve Zungul')
+    ]
+
+
+rookie = [
+    (1968, 'Willie Roy'),
+    (1969, 'Kaizer Motaung'),
+    (1970, 'Siegfried Stritzl'),
+    (1971, 'Jim Leeker'),
+    (1972, 'Randy Horton'),
+    (1973, 'Mike Winter'),
+    (1974, 'Kyle Rote Jr.'),
+    (1975, 'Douglas McMillan'),
+    (1976, 'Chari Bahr'),
+    (1977, 'Steve Pecher'),
+    (1978, 'Jim McAlister'),
+    (1979, 'Larry Hulcer'),
+    (1980, 'Jeff Durgan'),
+    (1981, 'Joe Morrone'),
+    (1982, 'Pedro DeBrito'),
+    (1983, 'Gregg Thompson'),
+    (1984, 'Roy Wegerle'),
+    ]
+
+   
