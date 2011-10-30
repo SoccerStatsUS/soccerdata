b97c0a709414adf4aac7c658719d59bab5aa1e6e
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-30 17:40:32 -0400

    Some work on scraping player bios from nasl jerseys.

--- scrapers/nasljerseys.py
+++ scrapers/nasljerseys.py
@@ -1,5 +1,7 @@
 
-from soccerdata.utils import scrape_soup
+import re
+
+from soccerdata.utils import scrape_soup, get_contents
 
 url = 'http://nasljerseys.com/Players/Players_Roster.htm'
 
@@ -16,11 +18,41 @@ def scrape_player_urls():
         except KeyError:
             print e
 
-    return urls
+    return ["http://nasljerseys.com/Players/%s" % e for e in urls]
 
-    import pdb; pdb.set_trace()
-    x = 5
+
+def scrape_player_bios():
+    l = []
+    for url in scrape_player_urls():
+        bio = scrape_player_bio(url)
+        if bio:
+            l.append(bio)
+    return l
+
+
+
+def scrape_player_bio(url):
+    soup = scrape_soup(url)
+    t = soup.find("table", "PlayersName")
+    if not t:
+        t = soup.find("table", "MsoTableGrid")
+
+    name = t.find("td").contents[0]
+    try:
+        born_string = t.find("td").contents[3].contents[0]
+    except:
+        import pdb; pdb.set_trace()
+    born_string = born_string.replace("Born:", '')
+    if not born_string.strip():
+        return {}
+
+    r = re.match("(?P<location>.*?)(?P<date>\w+ \d+, \d+)", born_string)
+    if r:
+        print r.groups()
+    else:
+        print "NO MATCH %s" % url
 
 
 if __name__ == "__main__":
-    print scrape_player_urls()
+    #print scrape_player_bio('http://www.nasljerseys.com/Players/W/West.Alan.htm')
+    print scrape_player_bios()
