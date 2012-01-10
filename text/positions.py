076cc561416c0182aa2e185996ea1b51e21b3fb4
chirs <chrisedgemon@gmail.com>
2012-01-10 16:48:49 -0500

    Clean up code for positions.py

--- text/positions.py
+++ text/positions.py
@@ -1,10 +1,9 @@
-# Should change this to positions.py
-
 import datetime
 import re
 
 from soccerdata.alias import get_team, get_name
 
+# Need to add in new positions.
 POSITIONS_PATH = '/home/chris/www/soccerdata/data/transactions/positions'
 
 class PositionParser(object):
@@ -19,7 +18,12 @@ class PositionParser(object):
         self.position = None
         self.team = None
 
-    def process_date_string(self, s):
+    def process_date_string(self, s, end=False):
+        # A date string is either a full date or just a year, depending on data quality.
+        # If the date is just a year, we call it the first day of the year if it is the start of a job,
+        # and the last day of the year if it is the end of a job.
+
+        # This is problematic with seasons like 1922-1923 in which multiple years are played.
         s = s.strip()
         if not s:
             return None
@@ -28,29 +32,28 @@ class PositionParser(object):
         if m:
             month, day, year = [int(e) for e in m.groups()]
         else:
-            month, day, year = 1, 1, int(s) # default to the first day of the year.
-        
-        return datetime.datetime(year, month, day)
+            if end == False:
+                month, day, year = 1, 1, int(s) 
+
+            else:
+                month, day, year = 12, 31, int(s) - 1
+
+        d = datetime.datetime(year, month, day)
 
 
     def process_line(self, line):
         # Does date handling and fills in empty fields if necessary.
         fields = line.split(",")
 
-        try:
-            person, position, team = [e.strip() for e in fields[:3]]
-        except:
-            import pdb; pdb.set_trace()
+        person, position, team = [e.strip() for e in fields[:3]]
 
         start = end = None
         if len(fields) > 3:
             start = self.process_date_string(fields[3])
         if len(fields) > 4:
-            end = self.process_date_string(fields[4])
-            # Set end at the end of the year if appropriate.
-            if end and end.month == 1 and end.day == 1:
-                end = datetime.datetime(end.year + 1, end.month, end.day) - datetime.timedelta(days=1)
+            end = self.process_date_string(fields[4], end=True)
 
+        # Set state variables if necessary.
         if person:
             self.person = person
         if position:
