b65db060127a0a34e77a27b6ef9fc93dcacb2ea6
chirs <chrisedgemon@gmail.com>
2012-01-14 23:35:27 -0500

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,29 @@
+
+SOURCES = [
+        'apsl',
+        'asl',
+        'nasl',
+        'nasl2',
+        'open_cup',
+        'mls',
+        'mls2',
+        'mls_reserve',
+        'usl',
+        'partial',
+        'ncaa',
+        'chris',
+        'usa',
+        'concacaf',
+        ]
+
+
+# Skipping these.
+l =    [ 
+        'fifa',
+        'uslsoccer',
+        'wiki',
+        'kicker',
+        'mediotiempo',
+        'cnnsi',
+        'eufootball',
+        ]
