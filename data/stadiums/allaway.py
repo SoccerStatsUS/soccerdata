7f1865dce4f4dcf56d1cb4d0337835d0d6c4b6d4
chirs <chrisedgemon@gmail.com>
2012-05-06 14:23:44 -0400

    Add some more stadiums.

---
+++ data/stadiums/allaway.py
@@ -0,0 +1,52 @@
+
+
+l = [
+        {
+        'stadium': 'Steel Field',
+        'location': 'Bethlehem, PA',
+        'opened': 1916,
+        'capacity': 1000,
+        },
+
+        {
+        'stadium': 'Metropolitan Oval',
+        'location': 'Brooklyn, NY',
+        'opened': 1925,
+        'capacity': 1000,
+        },
+
+        {
+        'stadium': 'Lusitano Stadium',
+        'location': 'Ludlow, MA',
+        'opened': 1918, #more?
+        'capacity': 3000,
+        },
+
+        {
+        'stadium': 'Gaelic Park',
+        'location': 'Bronx, NY',
+        'opened': 1924,
+        },
+
+        {
+        'stadium': 'Clark Field',
+        'location': 'East Newark, NJ',
+        'opened': 1885,
+        'capacity': 0,
+        },
+
+        {
+        'stadium': 'Mark\'s Stadium',
+        'location': 'Fall River, MA',
+        'opened': 1922,
+        'capacity': 15000,
+        },
+
+        {
+        'stadium': 'Starlight Park',
+        'location': 'Bronx, NY',
+        
+        'capacity': 1000,
+        },
+
+]
