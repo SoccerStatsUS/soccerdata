fd2be3e3ead8cb3df384d52dc5eba371438ef94f
chirs <chrisedgemon@gmail.com>
2012-05-23 06:08:19 -0400

    Create location folders for stadium data.

---
+++ data/stadiums/international/italy.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import datetime
+
+stadiums = [
+    {
+        'name': 'Stadio Luigi Ferraris',
+        'location': 'Genoa, Italy',
+        'opened': datetime.datetime(1997, 4, 11),
+        'capacity': 10000,
+        'cost': 26000000,
+        'architect': 'HOK Sport',
+        },
+    {
---
+++ data/stadiums/international/panama.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import datetime
+
+stadiums = [
+    {
+        'name': 'Estadio Rommel Fernandez',
+        'location': 'Panama City, Panama',
+        },
+    {
