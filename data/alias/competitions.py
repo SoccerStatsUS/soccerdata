469d5c8576a9e18e3020a2977d0ea7539a8e746e
chirs <chrisedgemon@gmail.com>
2012-09-25 11:25:56 -0400

    Work some more on competitions data.

--- data/alias/competitions.py
+++ data/alias/competitions.py
@@ -1,4 +1,6 @@
-competitions = {
+aliases = {}
+
+full_alias = {
     'USISL Premier League': 'USL Premier Developmental League',
     'USISL Select League': 'USL First Division',
     'USISL Professional League': 'USL Second Division',
@@ -11,10 +13,39 @@ competitions = {
 
 
     'USL Pro': 'USL Second Division',
-    'Domestic Tour': 'Friendly',
-    'International Tour': 'Friendly',
+    'International Friendly': 'Friendly International',
 }
 
+aliases.update(full_alias)
+
+# Don't want to completely delete these.
+partial_alias {
+    'Domestic Tour': 'Friendly',
+    'International Tour': 'Friendly',
+
+    'Bicentennial Cup': 'Friendly',
+    'Carlsberg Cup': 'Friendly',
+    'Carolina Challenge Cup': 'Friendly',
+    'Dynamo Charities Cup': 'Frinedly',
+    'Europac International': 'Friendly',
+    'Spring Cup Matches': 'Friendly',
+    'Sunshine International': 'Friendly',
+    'Toronto International': 'Friendly',
+    'Tournament of Champions': 'Friendly',
+    'Trans-Atlantic Challenge Cup': 'Friendly',
+
+    'Amistad Cup': 'Friendly International',
+
+    'International Cup': 'Friendly International',
+    'Joe Robbie Cup': 'Friendly International',
+    'Kirin Cup': 'Friendly International',
+    'Los Angeles Soccer Tournament': 'Friendly',
+    'Mexico City Tournament': 'Friendly International',
+    'Miami Cup': 'Friendly International',
+    'North American Nations Cup': 'Friendly International',
+    'Presidents Cup': 'Friendly International',
+    'Trinidad Tournament': 'Friendly International',
+    
 
 
 def get_competition(s):
--- text/competitions.py
+++ text/competitions.py
@@ -6,20 +6,41 @@ COMPETITIONS_DIR = '/home/chris/www/soccerdata/data/competitions/'
 
 def load_competitions():
     l = []
-    files = ['domestic']
+    files = ['new']
     for fn in files:
         p = os.path.join(COMPETITIONS_DIR, fn)
         f = open(p)
         for line in f:
+            if line.startswith('*'):
+                continue
+
             if line.strip():
                 fields = [e.strip() for e in line.split(';')]
-                name, abbreviation, ctype, scope, area = fields
+                try:
+                    name, abbreviation, code, international, ctype, level, scope, area = fields
+                except:
+                    import pdb; pdb.set_trace()
+
+                if international.strip() == 'international':
+                    ib = True
+                else:
+                    ib = False
+
+                if level == '':
+                    level = None
+                else:
+                    level = int(level)
+
+
                 l.append({
                         'name': name,
                         'abbreviation': abbreviation,
+                        'code': code,
+                        'international': ib,
                         'ctype': ctype,
+                        'level': level,
                         'scope': scope,
-                        'area': area,
+                        'area': area
                         })
     
 
