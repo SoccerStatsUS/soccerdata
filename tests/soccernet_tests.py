98d93f061394034c890f753377771da8b04d2732
chirs <chrisedgemon@gmail.com>
2012-10-24 15:49:39 -0400

    Add place tests.

--- tests/soccernet_tests.py
+++ tests/soccernet_tests.py
@@ -2,7 +2,12 @@
 
 from soccerdata.scrapers import soccernet
 
-
+"""
+For some reason tests are hanging on this.
 def test_soccernet_game():
+    soccernet.scrape_soccernet_game('http://soccernet.espn.go.com/match?id=336079&cc=5901', 'Major League Soccer')
+    import pdb; pdb.set_trace()
+
     pass
-    #soccernet.scrape_soccernet_game()
+    
+"""
