8afe1caa9c799238dd01210285f1058593d0d16d
chirs <chrisedgemon@gmail.com>
2013-05-16 01:15:23 -0400

    Add women game data.

---
+++ data/lists/awards/women.py
@@ -0,0 +1,107 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+
+d = {
+    'competition': 'Women\'s United Soccer Association',
+    'team_data': ['Champion'],
+    'champion': 'Champion',
+
+    'Champion': [
+        ('2001', 'Bay Area CyberRays'),
+        ('2002', 'Carolina Courage'),
+        ('2003', 'Washington Freedom'),
+        ],
+    'MVP': [
+        ('2001', 'Tiffeny Milbrett'),
+        ('2002', 'Marinette Pichon'),
+        ('2003', 'Maren Meinert'),
+        ],
+
+    'Defensive Player of the Year': [
+        ('2002', 'Danielle Slaton'),
+        ],
+
+    'Rookie of the Year': [
+        ('2002', 'Abby Wambach'),
+        ('2003', 'Christine Latham'),
+        ],
+
+    'Coach of the Year': [
+        ('2001', 'Ian Sawyers'),
+        ('2002', 'Mark Krikorian'),
+        ('2003', 'Pia Sundhage'),
+        ],
+
+    'Goalkeeper of the Year': [
+        ('2001', 'LaKeysia Beene'),
+        ('2002', 'Kristin Luckenbill'),
+        ('2003', 'Briana Scurry'),
+        ],
+
+    'Referee of the Year': [
+        ('2001', 'Sandra Hunt'),
+        ('2002', 'Kari Seitz'),
+        ('2003', 'Terry Vaughn'),
+        ],
+    
+     
+}
+
+d = {
+    'competition': 'Women\'s Professional Soccer',
+    'team_data': ['Champion'],
+    'champion': 'Champion',
+
+    'Champion': [
+        ('2009', 'Sky Blue FC'),
+        ('2010', 'FC Gold Pride'),
+        ('2011', 'Western New York Flash'),
+        ],
+
+    'Player of the Year': [
+        ('2009', 'Marta'),
+        ('2010', 'Marta'),
+        ('2011', 'Veronica Boquete'),
+        ],
+
+    'Coach of the Year': [
+        ('2009', 'Abner Rogers'),
+        ('2010', 'Paul Riley'),
+        ('2011', 'Paul Riley'),
+        ],
+
+    'Defender of the Year': [
+        ('2009', 'Amy LePeilbet'),
+        ('2010', 'Amy LePeilbet'),
+        ('2011', 'Whitney Engen'),
+        ],
+
+    'Rookie of the Year': [
+        ('2010', 'Ali Riley'),
+        ('2011', 'Christen Press'),
+        ],
+
+    'Sportswoman of the Year': [
+        ('2009', 'Christie Rampone'),
+        ('2010', 'Natalie Spilger'),
+        ('2011', 'Nikki Krzysik'),
+        ],
+}
+
+
+d = {
+    'competition': 'WPSL',
+    'team_data': ['Champion'],
+    'champion': 'Champion',
+
+    'Champion': [
+        ('2001', 'Bay Area CyberRays'),
+        ('2002', 'Carolina Courage'),
+        ('2003', 'Washington Freedom'),
+        ],
+    'MVP': [
+        ('2004', 'Siss'),
+        ]
+
+}
