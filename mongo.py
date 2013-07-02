4bae754eb555beab9180baf32b87197982d1dd67
chirs <chrisedgemon@gmail.com>
2013-07-02 12:10:12 -0400

    Add awards meta-listing, deploy.py, build/README.md; clean up mongo.py.

--- mongo.py
+++ mongo.py
@@ -1,15 +1,6 @@
-
-# Presumably we're going to be sending things into databases from mongo.
-# This is how we do it...
-
-# This file should probably be merged into build.
-
-# Need to check for duplicates!
-
+# Simple mongo functions.
 
 import pymongo
-
-
 
 connection = pymongo.Connection()
 soccer_db = connection.soccer
@@ -44,49 +35,3 @@ def generic_load(coll, func, delete=False):
         insert_rows(coll, func())
     else:
         insert_rows(coll, func)
-
-
-
-# Statto data
-# Statto can only be scraped up to 2008, 1, 1 (or maybe earlier?)
-
-
-
-# RSSSF Data
-
-# Move all of these into rsssf modules.
-
-def scrape_spain_rsssf(years=None):
-    from scrapers.rsssf import spain
-
-    if years is None:
-        years = range(2000, 2012)
-
-    delete_rows(soccer_db.rsssf_spain_games)
-    for year in years:
-        rows = spain.process_year(year)
-        insert_rows(soccer_db.rsssf_spain_games, rows)
-
-
-def scrape_mls_rsssf(years=None):
-    from scrapers.rsssf import usa
-
-    if years is None:
-        years = range(1996, 2009)
-
-    delete_rows(soccer_db.rsssf_mls_games)
-    for year in years:
-        rows = usa.process_year(year)
-        insert_rows(soccer_db.rsssf_mls_games, rows)
-
-
-def scrape_australia_rsssf(years=None):
-    from scrapers.rsssf import australia
-
-    if years is None:
-        years = range(2006, 2012)
-
-    delete_rows(soccer_db.rsssf_australia_games)
-    for year in years:
-        rows = australia.process_year(year)
-        insert_rows(soccer_db.rsssf_australia_games, rows)
